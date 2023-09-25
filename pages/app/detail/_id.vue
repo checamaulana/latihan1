a5ef811d54cb6f0b33434919aad45c70b02b3949
Checa Maulana <checamaulana10@gmail.com>
2023-09-25 21:47:53 +0700

    Dynamic Route

---
+++ pages/app/detail/_id.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <div v-if="article.id == paramId" class="mb-5 p-5">
+          <img class="max-w-2xl" :src="article.img" alt="Gambar Artikel" />
+          <h2>{{ article.title }}</h2>
+          <p>{{ article.content }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      paramId: this.$route.params.id,
+      articles: [
+        {
+          id: 0,
+          title: 'Judul Artikel 1',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?',
+          img: 'https://images.unsplash.com/photo-1649282806617-c51bb282899c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        },
+        {
+          id: 1,
+          title: 'Judul Artikel 2',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?',
+          img: 'https://images.unsplash.com/photo-1635732646038-0a1a2fe3f2d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+        },
+        {
+          id: 2,
+          title: 'Judul Artikel 3',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?',
+          img: 'https://images.unsplash.com/photo-1640006807976-a6127e9d6fa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+        },
+      ],
+    }
+  },
+
+  methods: {},
+}
+</script>
